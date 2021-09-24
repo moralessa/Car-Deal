edea349060df144778225dd2a575898c510508d2
Sam M <moralessa978@gmail.com>
2021-09-24 16:11:30 -0600

    third commit

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const parallax = document.getElementById("paralaxMotorHome");
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = 2650 -offset  * .7 + "px";
+    parallax.style.backgroundPositionY = 1400 -offset  * .5 + "px";
 })
+
+
